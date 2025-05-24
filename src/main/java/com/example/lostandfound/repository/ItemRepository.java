c11c7ab26039ba319431ff963225bc808245aecb
Radun111 <thesanyaraduni@gmail.com>
2025-05-24 11:58:03 +0530

    Added dynamic search query

--- src/main/java/com/example/lostandfound/repository/ItemRepository.java
+++ src/main/java/com/example/lostandfound/repository/ItemRepository.java
@@ -8,4 +8,11 @@ import java.util.List;
 public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByStatus(ItemStatus status); // Finds items by status
     List<Item> findByReportedBy(User user); // Finds items by reporter
+
+    @Query("SELECT i FROM Item i WHERE " +
+            "(:#{#criteria.status} IS NULL OR i.status = :#{#criteria.status}) AND " +
+            "(:#{#criteria.location} IS NULL OR i.locationFound LIKE %:#{#criteria.location}%) AND " +
+            "(:#{#criteria.nameContains} IS NULL OR i.name LIKE %:#{#criteria.nameContains}%)")
+    List<Item> searchItems(@Param("criteria") ItemSearchCriteria criteria);
+
 }
