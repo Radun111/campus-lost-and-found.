179ea9a6fa4087bdd30a0ed2271fed4d35c18410
Radun111 <thesanyaraduni@gmail.com>
2025-06-01 00:21:28 +0530

    Initialize Spring Boot project with JPA and Security dependencies

--- src/main/java/com/example/lostandfound/DemoApplication.java
+++ src/main/java/com/example/lostandfound/DemoApplication.java
@@ -2,12 +2,12 @@ package com.example.lostandfound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan("com.example.lostandfound.config")
 public class DemoApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
